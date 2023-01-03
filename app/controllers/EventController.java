2c6b6e5a57f54a2deb1c9f4766d41ac5129fb7dc
IronJoo <joanaferro92@gmail.com>
2023-01-03 19:28:59 +0000

    Developed view event.scala.html with corresponding event.css and EventController and route configuration Created css variables for colors and replaced all colors in the file with variables!!! Stylized <h2>, <h6> Redefined the size for <p> Created red buttons (both outlined and filled)

---
+++ app/controllers/EventController.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class EventController extends Controller {
+    public EventController() {
+    }
+
+    public Result event(){
+        return ok(views.html.event.render());
+    }
+}
